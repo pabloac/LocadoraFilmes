c42100e31ecf07607e908bbed08aef4dae8541a3
Pablo Cardoso <pabloac.ti@gmail.com>
2023-03-06 16:56:15 -0300

    Desenvolvimento de Upload de capa com Validação

--- LocadoraFilmes/Controllers/FilmeController.cs
+++ LocadoraFilmes/Controllers/FilmeController.cs
@@ -1,8 +1,11 @@
 ﻿using LocadoraFilmes.Models;
 using LocadoraFilmes.Repositorio;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
+using System.IO;
 
 namespace LocadoraFilmes.Controllers
 {
@@ -10,10 +13,14 @@ namespace LocadoraFilmes.Controllers
     {
 
         private readonly IFilmeRepositorio _filmeRepositorio;
+        private readonly IConfiguration _configuration;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public FilmeController(IFilmeRepositorio filmeRepositorio)
+        public FilmeController(IFilmeRepositorio filmeRepositorio, IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             _filmeRepositorio = filmeRepositorio;
+            _configuration = configuration;
+            _webHostEnvironment = webHostEnvironment;
         }
 
 
@@ -70,9 +77,48 @@ namespace LocadoraFilmes.Controllers
         {
             try
             {
-                _filmeRepositorio.Update(objeto);
+                string dirCapas = _configuration.GetSection("AppSettings").GetValue<string>("IMAGENS_CAPAS");
+                string dirServer = Path.Combine(_webHostEnvironment.WebRootPath, dirCapas);
 
-                return RedirectToAction(nameof(Index));
+
+
+                if (objeto.FotoCapa != null)
+                {
+                    long id = objeto.Id;//Nome do Arquivo
+
+                    //1- Checar Extensão (no momento apenas PNG)
+                    
+                    var extensao = objeto.FotoCapa.FileName.Split('.')[1];
+                    if (extensao.ToLower() != "png")
+                    {
+                        ModelState.AddModelError("name", string.Concat("Extensão de arquivo incorreta (somente PNG)"));
+                    }
+                    else
+                    {
+                        //Continuar Upload
+                        using (var stream = System.IO.File.Create(dirServer + "/teste.png"))
+                        {
+                            objeto.FotoCapa.CopyTo(stream);
+                        }
+
+                    }
+
+
+
+                }
+
+
+                if (ModelState.IsValid)
+                {
+
+                    _filmeRepositorio.Update(objeto);
+
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    return View(objeto);
+                }
             }
             catch
             {
--- LocadoraFilmes/Models/FilmeModel.cs
+++ LocadoraFilmes/Models/FilmeModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace LocadoraFilmes.Models
 {
@@ -14,6 +16,11 @@ namespace LocadoraFilmes.Models
 
         [Display(Name = "Classificação")]
         public int IdadeClassificacao { get; set; }
+
+
+        [NotMapped]
+        [Display(Name = "Foto da Capa")]
+        public IFormFile FotoCapa { get; set; }
 
 
     }
